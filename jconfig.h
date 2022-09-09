2889d54936dbc0bd0a8db73d8a26ecb3639a90e8
Xiang Xiao <xiaoxiang@xiaomi.com>
2022-09-09 19:39:53 +0800

    Update jconfig.h and jconfigint.h after merging
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- jconfig.h
+++ jconfig.h
@@ -4,10 +4,10 @@
 #define JPEG_LIB_VERSION  62
 
 /* libjpeg-turbo version */
-#define LIBJPEG_TURBO_VERSION  2.1.3
+#define LIBJPEG_TURBO_VERSION  2.1.4
 
 /* libjpeg-turbo version in integer form */
-#define LIBJPEG_TURBO_VERSION_NUMBER  2001003
+#define LIBJPEG_TURBO_VERSION_NUMBER  2001004
 
 /* Support arithmetic encoding */
 #define C_ARITH_CODING_SUPPORTED 1
@@ -20,7 +20,6 @@
 
 /* Use accelerated SIMD routines. */
 /* #undef WITH_SIMD */
-
 
 /*
  * Define BITS_IN_JSAMPLE as either
